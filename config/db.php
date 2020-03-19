958dcb2b4bf74b03f76ee38a5180ce3bdf42ca64
Mahesh K <maheshk@Maheshs-MacBook-Pro.local>
2020-03-19 17:16:39 +0530

    added migrations for user

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=sandbox_app',
     'username' => 'root',
-    'password' => '',
+    'password' => '@MyWork@',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
---
+++ migrations/m200319_110300_add_table_user_in_sandbox_app.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200319_110300_add_table_user_in_sandbox_app
+ */
+class m200319_110300_add_table_user_in_sandbox_app extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute("CREATE TABLE `sandbox_app`.`user` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `name` VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `username` VARCHAR(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `email` VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `age` INT(11) NOT NULL , `sex` VARCHAR(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `city` VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `state` VARCHAR(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `password` VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `status` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1=active, 0=inactive' , `deleted` TINYINT(1) NOT NULL DEFAULT '0' , `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `updated_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200319_110300_add_table_user_in_sandbox_app cannot be reverted.\n";
+
+        return false;
+    }
+
+}
