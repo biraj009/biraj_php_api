e2ba260c109e82b150e7a7d037a86bbb80a90790
Mahesh K <maheshk@Maheshs-MacBook-Pro.local>
2020-03-19 17:43:55 +0530

    added migrations for access token

--- migrations/m200319_110300_add_table_user_in_sandbox_app.php
+++ migrations/m200319_110300_add_table_user_in_sandbox_app.php
@@ -13,6 +13,7 @@ class m200319_110300_add_table_user_in_sandbox_app extends Migration
     public function safeUp()
     {
         $this->execute("CREATE TABLE `user` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `name` VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `username` VARCHAR(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `email` VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `age` INT(11) NOT NULL , `sex` VARCHAR(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `city` VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `state` VARCHAR(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `password` VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , `status` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1=active, 0=inactive' , `deleted` TINYINT(1) NOT NULL DEFAULT '0' , `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `updated_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+        $this->execute("ALTER TABLE `user` ADD `access_token` VARCHAR(150) NULL AFTER `email`, ADD UNIQUE `access_token` (`access_token`);");
 
     }
 
